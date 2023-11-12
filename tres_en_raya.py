9b4ef5a48a1d06ea4a892a9a225bf74871e8106e
Roberthdj <roberthdj183@gmail.com>
2023-11-12 18:08:17 -0500

    Tres en raya v3.0 (refactor 02)

--- tres_en_raya.py
+++ tres_en_raya.py
@@ -1,97 +1,105 @@
 
 from random import randrange
 
-board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-flag = True
 
-
-def display_board(board):
-    for row in range(len(board)):
+def mostrar_tablero(tablero):
+    for row in range(len(tablero)):
         print('+-------+-------+-------+')
         print('|       |       |       |', end='\n|')
-        for col in range(len(board[row])):
-            print('  ', str(board[row][col]), end='   |')
+        for col in range(len(tablero[row])):
+            print('  ', str(tablero[row][col]), end='   |')
         print('\n|       |       |       |')
     print('+-------+-------+-------+')
 
 
-def enter_move(board):
-    free_fields = make_list_of_free_fields(board)
+def movimiento_persona(tablero):
+    campos_libres = crear_lista_de_campos_libres(tablero)
+
     try:
-        move_to = int(input('Ingresa tu movimiento:'))
-        if move_to > 0 and move_to < 10:
-            for posicion in range(len(free_fields)):
-                row, column = free_fields[posicion]
-                if move_to == board[row][column]:
-                    board[row][column] = 'O'
-            display_board(board)
-            return victory_for(board, 'O')
+        seleccionar_celda = int(input('Ingresa tu movimiento:'))
+        if (seleccionar_celda > 0 and seleccionar_celda < 10) and (seleccionar_celda not in lista_de_celdas_seleccionadas):
+            for i in range(len(campos_libres)):
+                row, col = campos_libres[i]
+                if seleccionar_celda == tablero[row][col]:
+                    lista_de_celdas_seleccionadas.append(tablero[row][col])
+                    tablero[row][col] = 'O'
+
+            mostrar_tablero(tablero)
+            return victoria_para(tablero, 'O')
+
         else:
-            print('Ingrese un valor valido!')
-            enter_move(board)
+            print('Error!, ingrese un valor valido!...')
+            movimiento_persona(tablero)
             return True
     except:
-        print('Ingrese un valor valido!')
-        enter_move(board)
+        print('Error!, ingrese un valor valido!')
+        movimiento_persona(tablero)
         return True
 
 
-def make_list_of_free_fields(board):
-    list_of_free_fields = []
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col] != 'X' and board[row][col] != 'O':
-                list_of_free_fields.append((row, col))
-    return list_of_free_fields
+def crear_lista_de_campos_libres(tablero):
+    lista_de_campos_libres = []
+    for row in range(len(tablero)):
+        for col in range(len(tablero[row])):
+            if tablero[row][col] != 'X' and tablero[row][col] != 'O':
+                lista_de_campos_libres.append((row, col))
+    return lista_de_campos_libres
 
 
-def victory_for(board, sign):
-    free_fields = make_list_of_free_fields(board)
+def victoria_para(tablero, sign):
+    campos_libres = crear_lista_de_campos_libres(tablero)
 
 #   Comprobando diagonales
     contador_1 = contador_2 = 0
-    for pos in range(len(board)):
-        if sign == board[pos][pos]:
+    for pos in range(len(tablero)):
+        if sign == tablero[pos][pos]:
             contador_1 += 1
-        if sign == board[pos][2-pos]:
+        if sign == tablero[pos][2-pos]:
             contador_2 += 1
     if contador_1 == 3 or contador_2 == 3:
         print('Las ' + sign + ' han ganado!')
         return False
 
 #   Comprobando filas y columnas
-    for row in range(len(board)):
+    for row in range(len(tablero)):
         contador_1 = contador_2 = 0
-        for column in range(len(board[row])):
-            if sign == board[column][row]:
+        for column in range(len(tablero[row])):
+            if sign == tablero[column][row]:
                 contador_1 += 1
-            if sign == board[row][column]:
+            if sign == tablero[row][column]:
                 contador_2 += 1
         if contador_1 == 3 or contador_2 == 3:
             print('Las ' + sign + ' han ganado!')
             return False
 
 #   Comprobar empate
-    if len(free_fields) == 0:
+    if len(campos_libres) == 0:
         print('Ha sido un empate!')
         return False
 
     return True
 
 
-def draw_move(board):
-    if board[1][1] == 5:
-        board[1][1] = 'X'
-        display_board(board)
+def movimiento_maquina(tablero):
+    if tablero[1][1] == 5:
+        tablero[1][1] = 'X'
+        mostrar_tablero(tablero)
         return True
 
-    free_fields = make_list_of_free_fields(board)
-    number = randrange(len(free_fields))
-    row, col = free_fields[number]
-    board[row][col] = 'X'
-    display_board(board)
-    return victory_for(board, 'X')
+    campos_libres = crear_lista_de_campos_libres(tablero)
+    number = randrange(len(campos_libres))
+    row, col = campos_libres[number]
+    lista_de_celdas_seleccionadas.append(tablero[row][col])
+    tablero[row][col] = 'X'
+    mostrar_tablero(tablero)
+    return victoria_para(tablero, 'X')
 
 
-while flag:
-    flag = (draw_move(board) and enter_move(board))
+if __name__ == '__main__':
+
+    tablero = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    lista_de_celdas_seleccionadas = [5]
+    flag = True
+
+    while flag:
+        flag = (movimiento_maquina(tablero) and movimiento_persona(tablero))
